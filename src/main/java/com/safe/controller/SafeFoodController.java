8b4f932202bd479d08ff884e8044f3b635edd49f
조경은 <chokyungeun>
2019-11-04 15:15:07 +0900

    1104

--- src/main/java/com/safe/controller/SafeFoodController.java
+++ src/main/java/com/safe/controller/SafeFoodController.java
@@ -2,12 +2,12 @@ package com.safe.controller;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -31,14 +31,14 @@ public class SafeFoodController {
 		return "main";
 	}
 	
-	@GetMapping("/search.food")
+	@GetMapping("/list.food")
 	public String searchAll(Model model) {
 		List<Food> list = fservice.searchAll();
 		model.addAttribute("list", list);
 		return "search";
 	}
 
-	@RequestMapping(value = "/read.food")
+	@GetMapping("/read.food")
 	public String detail(Model model, int code) {
 		fservice.countUp(code);
 		Food f = fservice.selectOne(code);
@@ -47,7 +47,7 @@ public class SafeFoodController {
 		return "read";
 	}
 
-	@GetMapping(value = "/search.food")
+	@PostMapping(value = "/search.food")
 	public String search(Model model, String condition, String word) {
 		List<Food> list = fservice.search(condition, word);
 		model.addAttribute("list", list);
