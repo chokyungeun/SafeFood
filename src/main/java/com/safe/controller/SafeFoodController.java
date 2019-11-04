23b399c406d6c77ffc13877cecae1b27a0dd6668
yelee <yeeunlee94618@gmail.com>
2019-11-04 17:35:14 +0900

    asd

--- src/main/java/com/safe/controller/SafeFoodController.java
+++ src/main/java/com/safe/controller/SafeFoodController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.safe.algo.Algo;
 import com.safe.service.FoodService;
 import com.safe.vo.Food;
 
@@ -49,11 +50,107 @@ public class SafeFoodController {
 		return "read";
 	}
 
-	@PostMapping(value = "/search.food")
+	@PostMapping("/search.food")
 	public String search(Model model, String condition, String word) {
 		List<Food> list = fservice.search(condition, word);
 		model.addAttribute("list", list);
 		return "search";
 	}
+	
+	@GetMapping("/chart.food")
+	public String chart(Model model) {
+		List<Food> list = fservice.searchAll();
+		model.addAttribute("list", list);
+		model.addAttribute("msg", "인기순");
+		return "chart";
+	}
+	@PostMapping("/chart.food")
+	public String chart(Model model, String condition) {
+		List<Food> list = fservice.searchAll();
+		Algo a = new Algo();
+		System.out.println(condition);
+		if(condition.equals("popular") ) {
+			list = a.chartFoods(list);
+			model.addAttribute("msg", "인기순");
+		}else if(condition.equals("best")) {
+			list = a.goodFood(list);
+			model.addAttribute("msg", "권장순");
+		}else {
+			list = a.badFood(list);
+			model.addAttribute("msg", "비권장순");
+		}
+		model.addAttribute("list", list);
+		model.addAttribute("condition", list);
+		return "chart";
+	}
+	@GetMapping("/random.food")
+	public String random(Model model) {
+		List<Food> list = fservice.searchAll();
+		Algo a = new Algo();
+		Food b = a.randomFood(list);
+		model.addAttribute("b", b);
+		return "read";
+		
+	}
+	
+	
+	
+	/*
+	
+	public void random(HttpServletRequest req, HttpServletResponse res) {
+		HttpSession session = req.getSession(); // session ���
+		List<Food> list = bean.searchAll();
+		Algo a = new Algo();
+		Food b = a.randomFood(list);
+		req.setAttribute("b", b);
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/view/read.jsp");
+		try {
+			dispatcher.forward(req, res);// jsp�� forward ��Ŵ
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+		public void best(HttpServletRequest req, HttpServletResponse res) {
+		HttpSession session = req.getSession(); // session ���
+		List<Food> list = bean.searchAll();
+		Algo a = new Algo();
+		list = a.goodFood(list);
+		
+		req.setAttribute("list", list);
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/view/search.jsp");
+		try {
+			dispatcher.forward(req, res);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void bad(HttpServletRequest req, HttpServletResponse res) {
+		HttpSession session = req.getSession(); // session ���
+		List<Food> list = bean.searchAll();
+		Algo a = new Algo();
+		list = a.badFood(list);
+		
+		req.setAttribute("list", list);
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/view/search.jsp");
+		try {
+			dispatcher.forward(req, res);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	*/
+	
 
 }
